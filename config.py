30587487c05d54b7f36074c129889e8dca967bf7
Aleena Ahmed <sspaleena9779@gmail.com>
2025-08-25 23:14:19 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,11 @@
+# Update these to your MySQL credentials
+DB_CONFIG = {
+
+    'host': '10.101.8.49',
+    'user': 'dbuser',
+    'password': 'DBUser123',  # <- your password here
+    'database': 'game_portal',
+    'charset': 'utf8mb4'
+}
+
+SECRET_KEY = "supersecretkey123"
